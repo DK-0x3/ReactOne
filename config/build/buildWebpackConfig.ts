3b7f3d0c56ec274709587ec9c528a90da9226089
kezarev_dmitry <dkezarev516@gmail.com>
2024-10-10 22:15:14 +0500

    Прошел уроки 3, 4

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import {BuildOptions} from "./types/config";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import webpack from "webpack";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = options;
+
+    return {
+        mode,
+        devServer: isDev ? buildDevServer(options): undefined,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        devtool: isDev ? 'inline-source-map' : undefined,
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+    }
+}
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: number): string {
-    console.log('RANDOM_TEST');
-
-    return 'xxx';
-}

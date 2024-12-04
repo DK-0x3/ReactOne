2128ac3461e3ec1ca15a7d214abad004a464047d
kezarev_dmitry <dkezarev516@gmail.com>
2024-12-04 19:36:41 +0500

    Урок 31 Дорабатывает json-server

--- json-server/index.js
+++ json-server/index.js
@@ -1,48 +1,51 @@
-// eslint-disable-next-line
 const fs = require('fs');
-// eslint-disable-next-line
 const jsonServer = require('json-server');
-// eslint-disable-next-line
-const jwt = require('jsonwebtoken');
-// eslint-disable-next-line
 const path = require('path');
 
 const server = jsonServer.create();
-
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
+// Добавление парсинга JSON
+server.use(jsonServer.bodyParser);
+
 server.use(async (req, res, next) => {
-	await new Promise((res) => {
-		setTimeout(res, 800);
+	await new Promise((resolve) => {
+		setTimeout(resolve, 800); // Имитация задержки
 	});
 	next();
 });
 
+// Обработка маршрута /login
+server.post('/login', (req, res) => {
+	const { username, password } = req.body;
+
+	// Загрузка базы данных
+	const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+	const { users } = db;
+
+	// Поиск пользователя в базе данных
+	const userFromBd = users.find(
+		(u) => u.username === username && u.password === password,
+	);
+
+	if (userFromBd) {
+		return res.json(userFromBd); // Возврат данных пользователя
+	}
+
+	return res.status(400).json({ message: 'Authorization error' }); // Ошибка аутентификации
+});
+
+// Middleware для авторизации
 server.use((req, res, next) => {
 	if (!req.headers.authorization) {
-		console.log('sdfsdfsdsdfsdfsd');
 		return res.status(403).json({ message: 'AUTH ERROR' });
 	}
 	next();
 });
 
-// server.use(jsonServer.defaults());
+// Подключение роутера после кастомных маршрутов
+server.use(jsonServer.defaults());
 server.use(router);
-
-server.post('/login', (req, res) => {
-	const { username, password } = req.body;
-	const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
-	const { users } = db;
-
-	const userFromBd = users.find(
-		(u) => u.username === username && u.password === password,
-	);
-	if (userFromBd) {
-		return res.json(userFromBd);
-	}
-
-	return res.status(403).json({ message: 'Unauthorized error' });
-});
 
 server.listen(8000, () => {
 	console.log('Server started on port 8000');
